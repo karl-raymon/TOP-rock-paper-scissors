4c897683cb4ba4ae7537c171ee9d5fe298daef63
karl-raymon <devraymon31@gmail.com>
2026-02-08 19:20:39 +0800

    Updated UI and add and refactor functions

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+const buttons = document.querySelectorAll('.choices');
+const playerScoreDiv = document.querySelector('#player-score');
+const computerScoreDiv = document.querySelector('#computer-score');
+const winner = document.querySelector('.result');
+const MAX_SCORE = 5;
+let isGameOver = false;
+const resultToast = document.querySelector('#result-container');
+const resetBtn = document.querySelector('#reset');
+// buttons.forEach()
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => playGame(e));
+});
+
 function getComputerChoice() {
   const computerMove = Math.floor(Math.random() * 3) + 1;
   return computerMove === 1
@@ -9,11 +22,9 @@ function getComputerChoice() {
         : 'Invalid';
 }
 
-function getHumanChoice() {
-  return prompt(
-    'Please enter your choice here: \nChoices: Rock, Paper and Scissors',
-  )?.toLowerCase();
-}
+// function getHumanChoice(e) {
+//   return e.target.textContent.toLowerCase();
+// }
 
 let humanScore = 0;
 let computerScore = 0;
@@ -23,7 +34,7 @@ function playRound(humanSelection, computerSelection) {
   if (computerSelection === humanSelection) {
     console.log('tie');
   } else if (
-    (computerSelection === 'rock' && humanSelection === 'paper') ||
+    (computerSelection === 'rock' && humanSelection === 'scissors') ||
     (computerSelection === 'paper' && humanSelection === 'rock') ||
     (computerSelection === 'scissors' && humanSelection === 'paper')
   ) {
@@ -31,14 +42,39 @@ function playRound(humanSelection, computerSelection) {
   } else {
     humanScore++;
   }
+  displayScore();
 }
-playGame();
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice());
+// playGame();
+function playGame(e) {
+  const playerChoice = e.target.id.toLowerCase();
+  if (!isGameOver) {
+    playRound(playerChoice, getComputerChoice());
   }
-  console.log(computerScore, humanScore);
-  computerScore > humanScore
-    ? console.log('Player Lose')
-    : console.log('Player Wins');
+}
+displayScore();
+function displayScore() {
+  playerScoreDiv.textContent = humanScore;
+  computerScoreDiv.textContent = computerScore;
+
+  if (humanScore === MAX_SCORE || computerScore === MAX_SCORE) {
+    isGameOver = true;
+    console.log(isGameOver);
+    displayWinner();
+  }
+}
+
+function displayWinner() {
+  resultToast.style.display = 'flex';
+  winner.textContent = humanScore === 5 ? 'Player Wins' : 'Computer Wins';
+}
+
+resetBtn.addEventListener('click', resetGame);
+
+function resetGame() {
+  resultToast.style.display = 'none';
+  computerScore = 0;
+  humanScore = 0;
+  isGameOver = false;
+  winner.textContent = '';
+  displayScore();
 }
